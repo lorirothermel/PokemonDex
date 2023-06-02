6231d48dca3e7eb656f64b1550e1389546c32ecc
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-02 07:40:38 -0400

    Initial Commit

---
+++ PokemonDex/PokemonDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDexApp.swift
+//  PokemonDex
+//
+//  Created by Lori Rothermel on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
